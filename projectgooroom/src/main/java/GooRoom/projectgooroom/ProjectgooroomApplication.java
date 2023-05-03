b91671457b5b74544f851429ff0530ef3fe52aa3
Hwanvely <990706leo@gmail.com>
2023-05-03 14:46:45 +0900

    feat: 개발환경 설정

---
+++ projectgooroom/src/main/java/GooRoom/projectgooroom/ProjectgooroomApplication.java
@@ -0,0 +1,13 @@
+package GooRoom.projectgooroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectgooroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectgooroomApplication.class, args);
+	}
+
+}
---
+++ projectgooroom/src/test/java/GooRoom/projectgooroom/ProjectgooroomApplicationTests.java
@@ -0,0 +1,13 @@
+package GooRoom.projectgooroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectgooroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
