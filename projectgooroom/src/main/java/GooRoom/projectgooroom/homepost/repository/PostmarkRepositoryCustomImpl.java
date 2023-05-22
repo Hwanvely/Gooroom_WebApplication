2c5cf453ad4ad9e0609e1a8412d4ba479fe8b57a
juns1s <juns1s@naver.com>
2023-05-23 03:45:59 +0900

    fix: 게시글 페이징 수정

--- projectgooroom/src/main/java/GooRoom/projectgooroom/homepost/repository/PostmarkRepositoryCustomImpl.java
+++ projectgooroom/src/main/java/GooRoom/projectgooroom/homepost/repository/PostmarkRepositoryCustomImpl.java
@@ -1,13 +1,14 @@
 package GooRoom.projectgooroom.homepost.repository;
 
 import GooRoom.projectgooroom.homepost.domain.HomePost;
-import GooRoom.projectgooroom.homepost.domain.Postmark;
 import GooRoom.projectgooroom.homepost.dto.ListedPostDto;
 import GooRoom.projectgooroom.member.domain.Member;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,41 +30,47 @@ public class PostmarkRepositoryCustomImpl implements PostmarkRepositoryCustom{
     }
 
     @Override
-    public PageImpl<ListedPostDto> findAllByMember_Id(Long memberId, Pageable pageable) {
+    public Page<ListedPostDto> findAllByMember_Id(Long memberId, Pageable pageable) {
         List<Tuple> tuples = queryFactory
-                .select(homePost, member, postmark)
+                .select(homePost, homePost.member)
                 .from(postmark)
                 .join(postmark.homePost, homePost)
                 .join(postmark.member, member)
                 .where(member.id.eq(memberId))
                 .orderBy(homePost.lastEditTime.desc())
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize()* pageable.getPageNumber())
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset() * pageable.getPageSize())
                 .fetch();
+
+        JPAQuery<HomePost> query = queryFactory
+                .select(homePost)
+                .from(postmark)
+                .join(postmark.homePost, homePost)
+                .join(postmark.member, member)
+                .where(member.id.eq(memberId));
 
         List<HomePost> homePosts = new ArrayList<>();
         List<Member> members = new ArrayList<>();
-        List<Postmark> postmarks = new ArrayList<>();
 
         for (Tuple tuple : tuples) {
             HomePost homePost = tuple.get(0, HomePost.class);
             Member member = tuple.get(1, Member.class);
-            Postmark postmark = tuple.get(2, Postmark.class);
 
             homePosts.add(homePost);
             members.add(member);
-            postmarks.add(postmark);
         }
-        return getListedPostDtos(homePosts, members);
+        return PageableExecutionUtils.getPage(
+                getListedPostDtos(homePosts,members)
+                , pageable, query::fetchCount);
     }
 
-    private static PageImpl<ListedPostDto> getListedPostDtos(List<HomePost> homePosts, List<Member> members) {
+    private static List<ListedPostDto> getListedPostDtos(List<HomePost> homePosts, List<Member> members) {
         List<ListedPostDto> listedPostDtos = new ArrayList<>();
 
         for (int i = 0; i< members.size(); i++){
             listedPostDtos.add(new ListedPostDto(homePosts.get(i), members.get(i).getNickname(), members.get(i).getAge()));
         }
 
-        return new PageImpl<>(listedPostDtos);
+        return listedPostDtos;
     }
 }
