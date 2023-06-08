94a42f185f1cd83604130ec3a8c8846c251902ff
juns1s <juns1s@naver.com>
2023-06-08 22:48:32 +0900

    feat: site 도메인 추가

--- projectgooroom/src/main/java/GooRoom/projectgooroom/global/config/WebConfig.java
+++ projectgooroom/src/main/java/GooRoom/projectgooroom/global/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:3000",
                         "https://gooroom-frontend.vercel.app",
                         "https://gooroom-frontend-git-develop-clap-0.vercel.app",
-                        "https://www.gooroom.site"
+                        "https://www.gooroom.site",
+                        "https://www.gooroom.store"
                 )
                 .allowedMethods("*")
                 .allowedHeaders("*");
